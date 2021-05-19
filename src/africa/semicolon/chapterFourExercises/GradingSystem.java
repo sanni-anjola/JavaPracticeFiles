56e01155286f9475a23e992f73d126d83e73e7a8
Sanni Lateef Anjola <sanni.anjola@gmail.com>
2021-05-19 17:10:04 +0100

    Added Single Selection if statement to Grading System Class

--- src/africa/semicolon/chapterFourExercises/GradingSystem.java
+++ src/africa/semicolon/chapterFourExercises/GradingSystem.java
@@ -24,15 +24,31 @@ public class GradingSystem {
         Scanner scanner = new Scanner(System.in);
         int score = scanner.nextInt();
 
+//        if(score >= 90){
+//            System.out.println("A");
+//        }else if(score >= 80){
+//            System.out.println("B");
+//        }else if(score >= 70){
+//            System.out.println("C");
+//        }else if(score >= 60){
+//            System.out.println("D");
+//        }else {
+//            System.out.println("F");
+//        }
+
         if(score >= 90){
             System.out.println("A");
-        }else if(score >= 80){
+        }
+        if(score >= 80 && score <= 89){
             System.out.println("B");
-        }else if(score >= 70){
+        }
+        if(score >= 70 && score <= 79){
             System.out.println("C");
-        }else if(score >= 60){
+        }
+        if(score >= 60 && score <= 69){
             System.out.println("D");
-        }else {
+        }
+        if(score < 60) {
             System.out.println("F");
         }
     }
