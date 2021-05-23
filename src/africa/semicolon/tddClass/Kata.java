4ced1472a50dd65d7e4d21874f31f6c2cbaaa857
Sanni Lateef Anjola <sanni.anjola@gmail.com>
2021-05-23 08:48:39 +0100

    Added Grading System and TestDriller Price and Reseller Profit to Kata Class

--- src/africa/semicolon/tddClass/Kata.java
+++ src/africa/semicolon/tddClass/Kata.java
@@ -10,10 +10,59 @@ public class Kata {
             num = num / 10;
         }
         if(Math.abs(number)  != reversedNumber){
-            System.out.println("This number is not a palindrome");
+//            System.out.println("This number is not a palindrome");
             return false;
         }
-        System.out.println("This number is a palindrome");
+//        System.out.println("This number is a palindrome");
         return true;
+    }
+
+
+    public char whatGradeIs(int score) {
+
+        if(score >= 90){
+            return 'A';
+        }else if(score >= 80){
+            return 'B';
+        }else if(score >= 70){
+            return 'C';
+        }else if(score >= 60){
+            return 'D';
+        }else {
+            return 'F';
+        }
+    }
+//    TestDriller
+    public int totalAmountForTestDriller(int copies){
+        int amountPerDrill = 0;
+        if(copies > 0) {
+            if (copies >= 200) {
+                amountPerDrill = 800;
+            } else if (copies >= 100) {
+                amountPerDrill = 900;
+            } else if (copies >= 50) {
+                amountPerDrill = 1000;
+            } else if (copies >= 30) {
+                amountPerDrill = 1100;
+            } else if (copies >= 10) {
+                amountPerDrill = 1200;
+            } else if (copies >= 5) {
+                amountPerDrill = 1400;
+            } else {
+                amountPerDrill = 1500;
+            }
+        }
+        return copies * amountPerDrill;
+
+    }
+
+    public int resellersProfit(int copies){
+        final int AMOUNT_PER_DRILL = 2000;
+        int profit = 0;
+        if(copies > 0) {
+            int sellingPrice = copies * AMOUNT_PER_DRILL;
+            profit = sellingPrice - totalAmountForTestDriller(copies);
+        }
+        return profit;
     }
 }
--- test/africa/semicolon/tddClass/KataTest.java
+++ test/africa/semicolon/tddClass/KataTest.java
@@ -1,58 +1,153 @@
 package africa.semicolon.tddClass;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.assertFalse;
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.*;
 
 
 public class KataTest {
 
+    private Kata kata;
+
+    @BeforeEach
+    void InitializeKataBeforeEachTest(){
+        kata = new Kata();
+    }
+
     @Test
     void testThatKataReturnsTrueForThreeDigitPalindrome(){
-        Kata kata = new Kata();
-
         boolean res = kata.isPalindrome(989);
         assertTrue(res);
     }
 
     @Test
     void testThatKataReturnsTrueForFourDigitPalindrome(){
-        Kata kata = new Kata();
-
         boolean res = kata.isPalindrome(9889);
         assertTrue(res);
     }
 
     @Test
     void testThatKataReturnsTrueForSingleDigit(){
-        Kata kata = new Kata();
-
         boolean res = kata.isPalindrome(9);
         assertTrue(res);
     }
 
     @Test
     void testThatKataReturnsTrueForAnyNumberOfDigitPalindrome(){
-        Kata kata = new Kata();
-
         boolean res = kata.isPalindrome(712656217);
         assertTrue(res);
     }
 
     @Test
     void testThatKataReturnsTrueForPalindromicNegativeNumbers(){
-        Kata kata = new Kata();
-
         boolean res = kata.isPalindrome(-712656217);
         assertTrue(res);
     }
 
     @Test
     void testThatKataReturnsFalseForNonPalindromicNumbers(){
-        Kata kata = new Kata();
-
         boolean res = kata.isPalindrome(-7126567);
         assertFalse(res);
+    }
+
+    @Test
+    void testThatWhatGradeIsReturnsAFor90AndAbove(){
+        assertEquals('A', kata.whatGradeIs(96));
+    }
+
+    @Test
+    void testThatWhatGradeIsReturnsBFor80to89(){
+        assertEquals('B', kata.whatGradeIs(83));
+    }
+
+    @Test
+    void testThatWhatGradeIsReturnsCFor70to79(){
+        assertEquals('C', kata.whatGradeIs(70));
+    }
+
+    @Test
+    void testThatWhatGradeIsReturnsDFor60to69(){
+        assertEquals('D', kata.whatGradeIs(61));
+    }
+
+    @Test
+    void testThatWhatGradeIsReturnsFFor59AndBelow(){
+        assertEquals('F', kata.whatGradeIs(59));
+    }
+
+    @Test
+    void testThatTotalAmountForTestDrillerReturnsZeroForNegativeCopies(){
+        assertEquals(0, kata.totalAmountForTestDriller(-3));
+    }
+
+    @Test
+    void testThatTotalAmountForTestDrillerReturnsThePriceFor4CopiesOrLess(){
+        assertEquals(4500, kata.totalAmountForTestDriller(3));
+    }
+
+    @Test
+    void testThatTotalAmountForTestDrillerReturnsThePriceFor5to9Copies(){
+        assertEquals(11200, kata.totalAmountForTestDriller(8));
+    }
+
+    @Test
+    void testThatTotalAmountForTestDrillerReturnsThePriceFor10to29Copies(){
+        assertEquals(25200, kata.totalAmountForTestDriller(21));
+    }
+
+    @Test
+    void testThatTotalAmountForTestDrillerReturnsThePriceFor50to99Copies(){
+        assertEquals(77000, kata.totalAmountForTestDriller(77));
+    }
+
+    @Test
+    void testThatTotalAmountForTestDrillerReturnsThePriceFor100to199Copies(){
+        assertEquals(178200, kata.totalAmountForTestDriller(198));
+    }
+
+    @Test
+    void testThatTotalAmountForTestDrillerReturnsThePriceFor200CopiesOrAbove(){
+        assertEquals(240000, kata.totalAmountForTestDriller(300));
+    }
+
+    @Test
+    void testThatResellersProfitReturnedZeroForNegativeCopies(){
+        assertEquals(0, kata.resellersProfit(-7));
+    }
+
+    @Test
+    void testThatResellersProfitReturnedIsCorrectFor4CopiesOrLess(){
+        assertEquals(1000, kata.resellersProfit(2));
+    }
+
+    @Test
+    void testThatResellersProfitReturnedIsCorrectFor5To9Copies(){
+        assertEquals(3600, kata.resellersProfit(6));
+    }
+
+    @Test
+    void testThatResellersProfitReturnedIsCorrectFor10To29Copies(){
+        assertEquals(12000, kata.resellersProfit(15));
+    }
+
+    @Test
+    void testThatResellersProfitReturnedIsCorrectFor30To49Copies(){
+        assertEquals(36000, kata.resellersProfit(40));
+    }
+
+    @Test
+    void testThatResellersProfitReturnedIsCorrectFor50To99Copies(){
+        assertEquals(67000, kata.resellersProfit(67));
+    }
+
+    @Test
+    void testThatResellersProfitReturnedIsCorrectFor100To199Copies(){
+        assertEquals(211200, kata.resellersProfit(192));
+    }
+
+    @Test
+    void testThatResellersProfitReturnedIsCorrectFor200CopiesAndAbove(){
+        assertEquals(620400, kata.resellersProfit(517));
     }
 }
