53d3be27f13a3d95ada8d22fcc9a124b311b9fe5
Sanni Lateef Anjola <sanni.anjola@gmail.com>
2021-05-30 04:48:10 +0100

    Change the looping from sentinel to counter-controlled

--- src/africa/semicolon/chapterFiveExercises/TwelveDaysOfChristmasApp.java
+++ src/africa/semicolon/chapterFiveExercises/TwelveDaysOfChristmasApp.java
@@ -5,13 +5,19 @@ import java.util.Scanner;
 public class TwelveDaysOfChristmasApp {
     public static void main(String[] args) {
         TwelveDaysOfChristmas sing = new TwelveDaysOfChristmas();
-        Scanner scanner = new Scanner(System.in);
-        int userInput;
-
-        do {
-            System.out.println("Type between 1 and 12 for the Christmas Song or -1 to exit");
-            userInput = scanner.nextInt();
-            System.out.println(sing.sing(userInput));
-        }while (userInput != -1);
+        int counter = 1;
+//        Scanner scanner = new Scanner(System.in);
+//        int userInput;
+//
+//        do {
+//            System.out.println("Type between 1 and 12 for the Christmas Song or -1 to exit");
+//            userInput = scanner.nextInt();
+//            System.out.println(sing.sing(userInput));
+//        }while (userInput != -1);
+        while (counter <= 12){
+            System.out.println(sing.sing(counter));
+            System.out.println();
+            counter++;
+        }
     }
 }
