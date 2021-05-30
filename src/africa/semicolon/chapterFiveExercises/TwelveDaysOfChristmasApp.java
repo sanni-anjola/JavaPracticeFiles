27ea3601744a93e99ea4c74ea4ec8335bc878baa
Sanni Lateef Anjola <sanni.anjola@gmail.com>
2021-05-30 04:49:18 +0100

    Twelve days of Christmas sings properly

--- src/africa/semicolon/chapterFiveExercises/TwelveDaysOfChristmasApp.java
+++ src/africa/semicolon/chapterFiveExercises/TwelveDaysOfChristmasApp.java
@@ -6,14 +6,7 @@ public class TwelveDaysOfChristmasApp {
     public static void main(String[] args) {
         TwelveDaysOfChristmas sing = new TwelveDaysOfChristmas();
         int counter = 1;
-//        Scanner scanner = new Scanner(System.in);
-//        int userInput;
-//
-//        do {
-//            System.out.println("Type between 1 and 12 for the Christmas Song or -1 to exit");
-//            userInput = scanner.nextInt();
-//            System.out.println(sing.sing(userInput));
-//        }while (userInput != -1);
+
         while (counter <= 12){
             System.out.println(sing.sing(counter));
             System.out.println();
