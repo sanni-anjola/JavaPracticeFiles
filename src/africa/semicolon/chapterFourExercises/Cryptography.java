725ede98a6cd2432bfaf6835f26a2d655f250ba6
Sanni Lateef Anjola <sanni.anjola@gmail.com>
2021-06-07 22:24:49 +0100

    Added Factors of Numbers and NumberPyramid

---
+++ src/africa/semicolon/chapterFourExercises/Cryptography.java
@@ -0,0 +1,12 @@
+package africa.semicolon.chapterFourExercises;
+
+public class Cryptography {
+
+    public String encrypt(int number){
+        return "";
+    }
+
+    public String decrypt(int number){
+        return "";
+    }
+}
---
+++ test/africa/semicolon/chapterFourExercises/CryptographyTest.java
@@ -0,0 +1,12 @@
+package africa.semicolon.chapterFourExercises;
+
+import org.junit.jupiter.api.BeforeEach;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CryptographyTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+}
