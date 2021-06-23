20d72798d6c12e3e84635b3a7cce58a975980520
Sanni Lateef Anjola <sanni.anjola@gmail.com>
2021-06-23 07:51:18 +0100

    Refactored Student's Grade Multi-dimensional Array

--- src/africa/semicolon/chapterSevenExercises/StudentGrades.java
+++ src/africa/semicolon/chapterSevenExercises/StudentGrades.java
@@ -1,6 +1,5 @@
 package africa.semicolon.chapterSevenExercises;
 
-import java.util.Arrays;
 import java.util.Scanner;
 
 public class StudentGrades {
@@ -10,20 +9,14 @@ public class StudentGrades {
         int numberOfStudents = inputCollector.nextInt();
         System.out.print("Enter number of subject: ");
         int numberOfSubjects = inputCollector.nextInt();
-
         int[][] scores = new int[numberOfStudents][numberOfSubjects];
-        System.out.println("\n################ Collecting Scores ########################\n");
-        int score;
-        int student = 1;
-        for(int[] students : scores){
-            System.out.printf("Enter scores for student %d%n", student++);
-            System.out.println("____________________________________");
-            for (int subject = 0; subject < numberOfSubjects; subject++) {
-                System.out.printf("Enter score for subject %d%n", subject+1);
-                score = inputCollector.nextInt();
-                students[subject] = score;
-            }
-        }
+
+        fillScores(inputCollector, numberOfSubjects, scores);
+        displayScores(numberOfSubjects, scores);
+    }
+
+    private static void displayScores(int numberOfSubjects, int[][] scores) {
+        int student;
         System.out.println("\n################ Display Scores ########################\n");
         System.out.printf("%-15s", "Student's Name");
         for(int subject = 1; subject <= numberOfSubjects; subject++){
@@ -39,6 +32,20 @@ public class StudentGrades {
             }
             System.out.println();
         }
-        System.out.println(Arrays.deepToString(scores));
+    }
+
+    private static void fillScores(Scanner inputCollector, int numberOfSubjects, int[][] scores) {
+        System.out.println("\n################ Collecting Scores ########################\n");
+        int score;
+        int student = 1;
+        for(int[] students : scores){
+            System.out.printf("Enter scores for student %d%n", student++);
+            System.out.println("____________________________________");
+            for (int subject = 0; subject < numberOfSubjects; subject++) {
+                System.out.printf("Enter score for subject %d%n", subject+1);
+                score = inputCollector.nextInt();
+                students[subject] = score;
+            }
+        }
     }
 }
